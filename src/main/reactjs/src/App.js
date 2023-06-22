eef9ab974cd5282a8140f5e9c609f3315cad8f13
Hyun Oh <you@example.com>
2023-06-22 16:16:14 +0900

    커밋다시 사진추가 자동빌드 배포

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import car from './car5.png';
 
 
 function App() {
   return (
     <div className="App">
+      <img alt="" src={car} />
       <h2>Docker 배포 성공 기원! </h2>
     </div>
   );
